9b31b1b68d77eb160d41f552268451742f829234
Kaze Phake <phongsgvn@gmail.com>
2023-10-17 23:16:10 +0700

    Updated

--- Lab02/Additional/AddCheck.js
+++ Lab02/Additional/AddCheck.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
     $('input[type="checkbox"]').change(function() {
         if ($(this).is(':checked')) {
-            // Thực hiện hành động khi mục tiêu hoàn thành, ví dụ: thay đổi màu chữ
             $(this).closest('tr').css('color', 'green');
         } else {
-            // Thực hiện hành động khi mục tiêu chưa hoàn thành, ví dụ: đặt màu mặc định
             $(this).closest('tr').css('color', 'inherit');
         }
     });
